728bc6ccb6ce93c4d5d045567b464131c311840f
kristina <kristi_93@bk.ru>
2026-03-30 19:04:29 +0300

    log

--- sprint2/problems/logger/solution/my_logger.h
+++ sprint2/problems/logger/solution/my_logger.h
@@ -10,25 +10,27 @@
 #include <mutex>
 #include <thread>
 #include <syncstream>  
-#include <ctime>       
+#include <ctime>      
 
 using namespace std::literals;
 
-define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
+#define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
 
 class Logger {
     auto GetTime() const {
+        std::lock_guard<std::mutex> lock(ts_mutex_);
         if (manual_ts_) {
             return *manual_ts_;
         }
-
         return std::chrono::system_clock::now();
     }
 
-    auto GetTimeStamp() const {
+    std::string GetTimeStamp() const {
         const auto now = GetTime();
         const auto t_c = std::chrono::system_clock::to_time_t(now);
-        return std::put_time(std::localtime(&t_c), "%F %T");
+        std::stringstream ss;
+        ss << std::put_time(std::localtime(&t_c), "%F %T");
+        return ss.str();
     }
 
     // Для имени файла возьмите дату с форматом "%Y_%m_%d"
@@ -53,8 +55,12 @@ class Logger {
         }
     }
 
-    Logger() = default;
+    Logger() : current_file_stamp_("") {
+        CheckAndRotateFile();
+    }
+    
     Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
 
 public:
     static Logger& GetInstance() {
@@ -64,8 +70,8 @@ public:
 
     // Выведите в поток все аргументы.
     template<class... Ts>
-    void Log(const Ts&... args){
-		 std::lock_guard<std::mutex> lock(log_mutex_);
+    void Log(const Ts&... args) {
+        std::lock_guard<std::mutex> lock(log_mutex_);
         
         CheckAndRotateFile();
         
@@ -75,23 +81,22 @@ public:
         
         std::osyncstream sync_stream(log_file_);
         sync_stream << GetTimeStamp() << ": ";
-        ((sync_stream << args << " "), ...);
+        ((sync_stream << args), ...);
         sync_stream << std::endl;
-	}
+    }
 
     // Установите manual_ts_. Учтите, что эта операция может выполняться
     // параллельно с выводом в поток, вам нужно предусмотреть 
     // синхронизацию.
-    void SetTimestamp(std::chrono::system_clock::time_point ts){
-		std::lock_guard<std::mutex> lock(ts_mutex_);
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard<std::mutex> lock(ts_mutex_);
         manual_ts_ = ts;
-	}
+    }
 
 private:
     std::optional<std::chrono::system_clock::time_point> manual_ts_;
-	mutable std::mutex ts_mutex_; 
-	std::mutex log_mutex_;  
+    mutable std::mutex ts_mutex_; 
+    std::mutex log_mutex_;  
     std::ofstream log_file_;  
     std::string current_file_stamp_;  
-};
 };
