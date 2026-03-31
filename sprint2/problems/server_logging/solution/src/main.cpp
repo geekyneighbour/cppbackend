ed64b6f434bdfe1547126316bed11b72a905927f
kristina <kristi_93@bk.ru>
2026-03-31 18:34:20 +0300

    server_logging11

--- sprint2/problems/server_logging/solution/src/main.cpp
+++ sprint2/problems/server_logging/solution/src/main.cpp
@@ -26,10 +26,9 @@ namespace json = boost::json;
 
 BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 BOOST_LOG_ATTRIBUTE_KEYWORD(message, "Message", std::string)
-BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "additional_data", boost::json::value)
 
 namespace {
-    
+
     void InitLogging() {
         logging::add_common_attributes();
 
@@ -40,27 +39,24 @@ namespace {
                         << "{"
                         << "\"timestamp\":\""
                         << expr::format_date_time(timestamp, "%Y-%m-%dT%H:%M:%S.%f")
-                        << "\","
+                        << "\"," 
                         << "\"data\":"
                         << expr::if_(expr::has_attr(additional_data))
                                [expr::stream << additional_data]
                                .else_
                                [expr::stream << "{}"]
-                        << ","
-                        << "\"message\":\""
+                        << ",\"message\":\""
                         << message
                         << "\""
                         << "}"
                 ));
     }
 
-    // Запускает функцию fn на n потоках, включая текущий
     template <typename Fn>
     void RunWorkers(unsigned n, const Fn& fn) {
         n = std::max(1u, n);
         std::vector<std::jthread> workers;
         workers.reserve(n - 1);
-        // Запускаем n-1 рабочих потоков, выполняющих функцию fn
         while (--n) {
             workers.emplace_back(fn);
         }
@@ -71,20 +67,18 @@ namespace {
 
 int main(int argc, const char* argv[]) {
     InitLogging();
+
     if (argc != 2) {
         std::cerr << "Usage: game_server <game-config-json>"sv << std::endl;
         return EXIT_FAILURE;
     }
-    
+
     try {
-        // 1. Загружаем карту из файла и построить модель игры
         model::Game game = json_loader::LoadGame(argv[1]);
 
-        // 2. Инициализируем io_context
         const unsigned num_threads = std::thread::hardware_concurrency();
         net::io_context ioc(num_threads);
 
-        // 3. Добавляем асинхронный обработчик сигналов SIGINT и SIGTERM
         net::signal_set signals(ioc, SIGINT, SIGTERM);
         signals.async_wait([&ioc](auto, auto) {
             json::object data_obj;
@@ -96,49 +90,48 @@ int main(int argc, const char* argv[]) {
                 << "server exited";
             ioc.stop();
         });
-        
-        // 4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
+
         http_handler::RequestHandler handler{game};
         http_handler::LoggingRequestHandler<http_handler::RequestHandler> logging_handler{handler};
-        
-        // 5. Запустить обработчик HTTP-запросов, делегируя их обработчику запросов
+
         const auto address = net::ip::make_address("0.0.0.0");
         constexpr unsigned short port = 8080;
 
-        http_server::ServeHttp(ioc, {address, port}, logging_handler);
-        std::cout << "Server started" << std::endl;
-        json::object data_obj;
-        data_obj["port"] = port;
-        data_obj["address"] = address.to_string();
-        json::value data = std::move(data_obj);
 
-        BOOST_LOG_TRIVIAL(info)
-            << boost::log::add_value(additional_data, data)
-            << "server started";
+        {
+            json::object start_data;
+            start_data["event"] = "server_started";
+            start_data["address"] = address.to_string();
+            start_data["port"] = port;
+            std::cout << json::serialize(start_data) << std::endl;
 
-        // 6. Запускаем обработку асинхронных операций
-        RunWorkers(std::max(1u, num_threads), [&ioc] {
+            BOOST_LOG_TRIVIAL(info)
+                << boost::log::add_value(additional_data, start_data)
+                << "server started";
+        }
+
+
+        RunWorkers(std::max(1u, num_threads), [&ioc, &logging_handler, &address, port] {
+            http_server::ServeHttp(ioc, {address, port}, logging_handler);
             ioc.run();
         });
-        
+
+
         json::object exit_data_obj;
         exit_data_obj["code"] = 0;
-        json::value exit_data = std::move(exit_data_obj);
-
         BOOST_LOG_TRIVIAL(info)
-            << boost::log::add_value(additional_data, exit_data)
+            << boost::log::add_value(additional_data, exit_data_obj)
             << "server exited";
-        
+
     } catch (const std::exception& e) {
         json::object data_obj;
         data_obj["code"] = EXIT_FAILURE;
         data_obj["exception"] = e.what();
-        json::value data = std::move(data_obj);
-        
         BOOST_LOG_TRIVIAL(error)
-            << boost::log::add_value(additional_data, data)
+            << boost::log::add_value(additional_data, data_obj)
             << "server exited with error";
         return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
