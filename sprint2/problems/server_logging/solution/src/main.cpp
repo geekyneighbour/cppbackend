2a810c33e9273ba5e22dbd5ea66e3be6e08a5aa0
kristina <kristi_93@bk.ru>
2026-04-02 18:39:48 +0300

    server_logging29

--- sprint2/problems/server_logging/solution/src/main.cpp
+++ sprint2/problems/server_logging/solution/src/main.cpp
@@ -1,9 +1,8 @@
 #include "sdk.h"
+
 #include <boost/asio/io_context.hpp>
-#include <iostream>
-#include <thread>
 #include <boost/asio/signal_set.hpp>
-#include <csignal>
+#include <boost/json.hpp>
 
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
@@ -12,7 +11,10 @@
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/attributes.hpp>
 #include <boost/log/utility/manipulators/add_value.hpp>
-#include <boost/json.hpp>
+
+#include <iostream>
+#include <thread>
+#include <csignal>
 
 #include "json_loader.h"
 #include "request_handler.h"
@@ -25,15 +27,19 @@ namespace logging = boost::log;
 namespace expr = boost::log::expressions;
 namespace json = boost::json;
 
+
 BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
 BOOST_LOG_ATTRIBUTE_KEYWORD(message, "Message", std::string)
+BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData", json::value)
 
 namespace {
+
 
 void InitLogging() {
     logging::add_common_attributes();
 
     logging::add_console_log(std::cout,
+        logging::keywords::auto_flush = true,  
         logging::keywords::format =
         (
             expr::stream
@@ -53,11 +59,13 @@ void InitLogging() {
         ));
 }
 
+
 template <typename Fn>
 void RunWorkers(unsigned n, const Fn& fn) {
     n = std::max(1u, n);
     std::vector<std::jthread> workers;
     workers.reserve(n - 1);
+
     while (--n) {
         workers.emplace_back(fn);
     }
@@ -68,18 +76,19 @@ void RunWorkers(unsigned n, const Fn& fn) {
 
 int main(int argc, const char* argv[]) {
     InitLogging();
-	boost::log::core::get()->add_global_attribute("Flush", boost::log::attributes::current_thread_id());
-    boost::log::core::get()->flush();
+
     if (argc != 3) {
         std::cerr << "Usage: game_server <game-config-json> <static-files-directory>"sv << std::endl;
         return EXIT_FAILURE;
     }
 
     try {
+
         model::Game game = json_loader::LoadGame(argv[1]);
 
         const unsigned num_threads = std::thread::hardware_concurrency();
         net::io_context ioc(num_threads);
+
 
         net::signal_set signals(ioc, SIGINT, SIGTERM);
         signals.async_wait([&ioc](auto, auto) {
@@ -93,33 +102,27 @@ int main(int argc, const char* argv[]) {
             ioc.stop();
         });
 
+
         http_handler::RequestHandler handler{game, argv[2]};
         http_handler::LoggingRequestHandler<http_handler::RequestHandler> logging_handler{handler};
 
         const auto address = net::ip::make_address("0.0.0.0");
         constexpr unsigned short port = 8080;
 
+
         {
             json::object start_data;
-            start_data["event"] = "server_started";
             start_data["address"] = address.to_string();
             start_data["port"] = port;
 
-            std::string message = "Server has started..."; 
-
-
-    BOOST_LOG_TRIVIAL(info)
-        << boost::log::add_value(additional_data, start_data)
-        << message;
-
-
-    std::cout << message << std::endl;
-    std::cout.flush();
+            BOOST_LOG_TRIVIAL(info)
+                << boost::log::add_value(additional_data, start_data)
+                << "server started";
         }
+
 
         http_server::ServeHttp(ioc, {address, port}, logging_handler);
 
-        //std::cout << "Hello! Server is //starting at port " << port << //std::endl;
 
         RunWorkers(std::max(1u, num_threads), [&ioc] {
             ioc.run();
