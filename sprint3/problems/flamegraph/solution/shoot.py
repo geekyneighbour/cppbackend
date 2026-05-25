1a00c716db99dbf6be93b8939b61ea141ad81f38
kristina <kristi_93@bk.ru>
2026-05-25 20:57:20 +0300

    flamegraph

---
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -0,0 +1,83 @@
+import argparse
+import os
+import random
+import shlex
+import signal
+import subprocess
+import time
+
+AMMUNITION = [
+    'localhost:8080/api/v1/maps',
+    'localhost:8080/api/v1/maps/map1',
+    'localhost:8080/api/v1/maps/map2',
+]
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', type=str, help='Command to start the server')
+    return parser.parse_args()
+
+def start_server(server_command):
+    args = shlex.split(server_command)
+    return subprocess.Popen(args)
+
+def start_perf(server_pid):
+    perf_command = f"perf record -g -p {server_pid} -o perf.data"
+    args = shlex.split(perf_command)
+    return subprocess.Popen(args)
+
+def shoot(ammo):
+    with open(os.devnull, 'w') as devnull:
+        subprocess.Popen(['curl', '-s', ammo], stdout=devnull, stderr=devnull)
+
+def make_shots():
+    for _ in range(50):
+        ammo = random.choice(AMMUNITION)
+        shoot(ammo)
+        time.sleep(0.1)
+
+def stop_process(proc, signum=signal.SIGTERM):
+    if proc.poll() is None:
+        proc.send_signal(signum)
+        proc.wait()
+
+def generate_flamegraph():
+    p1 = subprocess.Popen(['perf', 'script', '-i', 'perf.data'], stdout=subprocess.PIPE)
+    
+	FlameGraph (лежит рядом)
+    p2 = subprocess.Popen(['./FlameGraph/stackcollapse-perf.pl'], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close() # Разрешаем p1 получать SIGPIPE
+    
+    with open('graph.svg', 'w') as svg_file:
+        p3 = subprocess.Popen(['./FlameGraph/flamegraph.pl'], stdin=p2.stdout, stdout=svg_file)
+        p2.stdout.close()
+        p3.wait()
+
+def main():
+    args = parse_args()
+
+    print("Starting server...")
+    server_proc = start_server(args.server)
+
+    time.sleep(0.5)
+
+    print(f"Starting perf record for server PID: {server_proc.pid}...")
+    perf_proc = start_perf(server_proc.pid)
+    
+    time.sleep(0.5)
+
+    print("Shooting ammunition...")
+    make_shots()
+
+    print("Stopping perf record...")
+    stop_process(perf_proc, signal.SIGINT)
+
+    print("Stopping server...")
+    stop_process(server_proc, signal.SIGTERM)
+
+    print("Generating flamegraph (graph.svg)...")
+    generate_flamegraph()
+    print("Done!")
+
+if __name__ == '__main__':
+    main()
---
+++ sprint3/problems/flamegraph/solution/src/request_handler.h
@@ -0,0 +1,153 @@
+#pragma once
+#include "http_server.h"
+#include "model.h"
+#include <boost/json.hpp>
+#include <string_view>
+
+namespace http_handler {
+    namespace beast = boost::beast;
+    namespace http = beast::http;
+    namespace json = boost::json;
+
+    class RequestHandler {
+    public:
+        explicit RequestHandler(model::Game& game)
+            : game_{ game } {
+        }
+
+        RequestHandler(const RequestHandler&) = delete;
+        RequestHandler& operator=(const RequestHandler&) = delete;
+
+        template <typename Body, typename Allocator, typename Send>
+        void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+            using namespace std::literals;
+
+            auto const bad_request = [&](std::string_view message) {
+                http::response<http::string_body> res{ http::status::bad_request, req.version() };
+                res.set(http::field::content_type, "application/json");
+
+                json::object error;
+                error["code"] = "badRequest";
+                error["message"] = std::string(message);
+                res.body() = json::serialize(error);
+                res.prepare_payload();
+                send(std::move(res));
+                };
+
+            auto const not_found = [&]() {
+                http::response<http::string_body> res{ http::status::not_found, req.version() };
+                res.set(http::field::content_type, "application/json");
+
+                json::object error;
+                error["code"] = "mapNotFound";
+                error["message"] = "Map not found";
+                res.body() = json::serialize(error);
+                res.prepare_payload();
+                send(std::move(res));
+                };
+
+            std::string path = std::string(req.target());
+
+            if (req.method() == http::verb::get &&
+                (path == "/api/v1/maps" || path.starts_with("/api/v1/maps/"))) {
+
+                if (path == "/api/v1/maps") {
+                    json::array result;
+
+                    for (const auto& map : game_.GetMaps()) {
+                        json::object obj;
+                        obj["id"] = std::string(*map.GetId());
+                        obj["name"] = map.GetName();
+                        result.push_back(obj);
+                    }
+
+                    http::response<http::string_body> res{ http::status::ok, req.version() };
+                    res.set(http::field::content_type, "application/json");
+                    res.body() = json::serialize(result);
+                    res.prepare_payload();
+
+                    return send(std::move(res));
+                }
+
+                std::string id = path.substr(std::string("/api/v1/maps/").size());
+
+                const auto* map = game_.FindMap(model::Map::Id{ id });
+
+                if (!map) {
+                    return not_found();
+                }
+
+                json::object obj;
+                obj["id"] = std::string(*map->GetId());
+                obj["name"] = map->GetName();
+
+                
+                obj["roads"] = SerializeRoads(map->GetRoads());
+                obj["buildings"] = SerializeBuildings(map->GetBuildings());
+                obj["offices"] = SerializeOffices(map->GetOffices());
+
+           
+
+                http::response<http::string_body> res{ http::status::ok, req.version() };
+                res.set(http::field::content_type, "application/json");
+                res.body() = json::serialize(obj);
+                res.prepare_payload();
+
+                return send(std::move(res));
+            }
+
+            return bad_request("Bad request");
+        }
+
+    private:
+        model::Game& game_;
+
+        json::array SerializeRoads(const std::vector<model::Road>& map_roads) {
+            json::array roads;
+            for (const auto& r : map_roads) {
+                json::object road;
+                road["x0"] = r.GetStart().x;
+                road["y0"] = r.GetStart().y;
+
+                if (r.IsHorizontal()) {
+                    road["x1"] = r.GetEnd().x;
+                }
+                else {
+                    road["y1"] = r.GetEnd().y;
+                }
+
+                roads.push_back(road);
+            }
+            return roads;
+        }
+        json::array SerializeBuildings(const std::vector<model::Building>& map_buildings) {
+            json::array buildings;
+            for (const auto& b : map_buildings) {
+                json::object building;
+                auto bounds = b.GetBounds();
+                building["x"] = bounds.position.x;
+                building["y"] = bounds.position.y;
+                building["w"] = bounds.size.width;
+                building["h"] = bounds.size.height;
+                buildings.push_back(building);
+            }
+            return buildings;
+        }
+        json::array SerializeOffices(const std::vector<model::Office>& map_offices) {
+            json::array offices;
+            for (const auto& o : map_offices) {
+                json::object office;
+                office["id"] = std::string(*o.GetId());
+                office["x"] = o.GetPosition().x;
+                office["y"] = o.GetPosition().y;
+                office["offsetX"] = o.GetOffset().dx;
+                office["offsetY"] = o.GetOffset().dy;
+                offices.push_back(office);
+            }
+            return offices;
+        }
+
+
+    };
+
+}  // namespace http_handler
