0c9f0a5b3e4e7d16475f87f04496c0aba69795b2
kristina <kristi_93@bk.ru>
2026-06-16 19:33:16 +0300

    gen_objects_35

--- sprint3/problems/gen_objects/solution/src/loot_generator.cpp
+++ sprint3/problems/gen_objects/solution/src/loot_generator.cpp
@@ -5,24 +5,33 @@
 
 namespace loot_gen {
 
-unsigned LootGenerator::Generate(TimeInterval time_delta, unsigned loot_count,
-                                 unsigned looter_count) {
+unsigned LootGenerator::Generate(TimeInterval time_delta,
+                                 unsigned loot_count,
+                                 unsigned looter_count)
+{
     time_without_loot_ += time_delta;
 
-    const unsigned loot_shortage = loot_count > looter_count ? 0u : looter_count - loot_count;
-    
-    const double ratio = std::chrono::duration<double>{time_without_loot_} / base_interval_;
-    
-    const double probability
-        = std::clamp((1.0 - std::pow(1.0 - probability_, ratio)) * random_generator_(), 0.0, 1.0);
-    
-    const unsigned generated_loot = static_cast<unsigned>(std::round(loot_shortage * probability));
-    
-    if (generated_loot > 0) {
-        time_without_loot_ = {};
+    unsigned generated = 0;
+
+    while (time_without_loot_ >= base_interval_) {
+        time_without_loot_ -= base_interval_;
+
+        if (random_generator_() < probability_) {
+            ++generated;
+        }
     }
-    
-    return generated_loot;
+
+    unsigned loot_shortage =
+        (loot_count < looter_count)
+        ? (looter_count - loot_count)
+        : 0;
+
+    if (generated > loot_shortage) {
+        generated = loot_shortage;
+    }
+
+    return generated;
+}
 }
 
 } // namespace loot_gen
