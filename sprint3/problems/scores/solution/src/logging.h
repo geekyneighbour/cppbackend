d19d5c034762eaed2c87a28977c365d898374b79
kristina <kristi_93@bk.ru>
2026-06-24 04:40:37 +0300

    final_4

--- sprint3/problems/scores/solution/src/logging.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <boost/log/attributes.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/json.hpp>
-
-BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData", boost::json::value)
