f9d486f661918eab073c59778b2ee41e875341ac
kristina <kristi_93@bk.ru>
2026-07-05 13:07:59 +0300

    leave_game_7

--- sprint4/problems/leave_game/solution/src/model_serialization.h
+++ sprint4/problems/leave_game/solution/src/model_serialization.h
@@ -6,6 +6,7 @@
 #include <boost/serialization/optional.hpp>
 #include <boost/serialization/shared_ptr.hpp>
 #include <boost/serialization/utility.hpp>
+#include <boost/serialization/unique_ptr.hpp>
 
 #include "model.h"
 #include "geom.h"
@@ -13,29 +14,29 @@
 namespace geom {
 
 template <typename Archive>
-void serialize(Archive& ar, Point2D& point, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, Point2D& point, const unsigned) {
     ar& point.x;
     ar& point.y;
 }
 
 template <typename Archive>
-void serialize(Archive& ar, Vec2D& vec, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, Vec2D& vec, const unsigned) {
     ar& vec.x;
     ar& vec.y;
 }
 
-}  // namespace geom
+} // namespace geom
 
 namespace model {
 
 template <typename Archive>
-void serialize(Archive& ar, FoundObject& obj, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, FoundObject& obj, const unsigned) {
     ar& *obj.id;
     ar& obj.type;
 }
 
 template <typename Archive>
-void serialize(Archive& ar, LostObject& obj, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, LostObject& obj, const unsigned) {
     ar& obj.id;
     ar& obj.type;
     ar& obj.pos;
@@ -43,19 +44,19 @@ void serialize(Archive& ar, LostObject& obj, [[maybe_unused]] const unsigned ver
 }
 
 template <typename Archive>
-void serialize(Archive& ar, Direction& dir, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, Direction& dir, const unsigned) {
     int d = static_cast<int>(dir);
     ar& d;
     dir = static_cast<Direction>(d);
 }
 
 template <typename Archive>
-void serialize(Archive& ar, BagItem& item, [[maybe_unused]] const unsigned version) {
+void serialize(Archive& ar, BagItem& item, const unsigned) {
     ar& item.id;
     ar& item.type;
 }
 
-}  // namespace model
+} // namespace model
 
 namespace serialization {
 
@@ -68,29 +69,31 @@ public:
         , name_(dog.GetName())
         , pos_(dog.GetPos().x, dog.GetPos().y)
         , bag_capacity_(dog.GetBagCapacity())
-        , speed_{dog.GetSpeed().x, dog.GetSpeed().y}  
+        , speed_{dog.GetSpeed().x, dog.GetSpeed().y}
         , direction_(dog.GetDirection())
-        , score_(dog.GetScore()) {
+        , score_(dog.GetScore())
+    {
         for (const auto& item : dog.GetBagContent()) {
             bag_content_.push_back(item);
         }
     }
 
-    [[nodiscard]] model::Dog Restore() const {
+    model::Dog Restore() const {
         model::Dog dog{model::Dog::Id{id_}, name_, {pos_.x, pos_.y}, bag_capacity_};
+
         dog.SetSpeed({speed_.x, speed_.y});
         dog.SetDirection(direction_);
         dog.SetScore(score_);
+
         for (const auto& item : bag_content_) {
-            if (!dog.PutToBag(item)) {
-                throw std::runtime_error("Failed to put bag content");
-            }
+            dog.PutToBag(item);
         }
+
         return dog;
     }
 
     template <typename Archive>
-    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+    void serialize(Archive& ar, const unsigned) {
         ar& id_;
         ar& name_;
         ar& pos_;
@@ -100,7 +103,7 @@ public:
         ar& score_;
         ar& bag_content_;
     }
-    
+
     uint64_t GetDogId() const { return id_; }
 
 private:
@@ -117,26 +120,26 @@ private:
 class LostObjectRepr {
 public:
     LostObjectRepr() = default;
-    
+
     explicit LostObjectRepr(const model::LostObject& obj)
         : id_(obj.id)
         , type_(obj.type)
         , pos_(obj.pos.x, obj.pos.y)
-        , value_(obj.value) {
-    }
-    
+        , value_(obj.value)
+    {}
+
     model::LostObject Restore() const {
         return model::LostObject{type_, {pos_.x, pos_.y}, value_, id_};
     }
-    
+
     template <typename Archive>
-    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+    void serialize(Archive& ar, const unsigned) {
         ar& id_;
         ar& type_;
         ar& pos_;
         ar& value_;
     }
-    
+
 private:
     size_t id_ = 0;
     size_t type_ = 0;
@@ -144,77 +147,73 @@ private:
     int value_ = 0;
 };
 
-
 class GameSessionRepr {
 public:
     GameSessionRepr() = default;
-    
+
     explicit GameSessionRepr(const model::GameSession& session)
         : map_id_(*session.GetMap()->GetId())
         , next_loot_id_(session.GetNextLootId())
-        , next_player_id_(session.GetNextPlayerId()) {  
+        , next_player_id_(session.GetNextPlayerId())
+    {
         for (const auto& dog : session.GetDogs()) {
             dogs_.push_back(DogRepr(*dog));
         }
+
         for (const auto& obj : session.GetLostObjects()) {
             lost_objects_.push_back(LostObjectRepr(obj));
         }
     }
-    
+
+    void Restore(model::Game& game,
+                 std::unordered_map<uint64_t, model::Dog*>& dog_id_map) const {
+
+        auto* map = game.FindMap(model::Map::Id{map_id_});
+        if (!map) {
+            throw std::runtime_error("Map not found: " + map_id_);
+        }
+
+        auto& session = game.FindOrCreateSession(map);
+
+        session.SetNextLootId(next_loot_id_);
+        session.SetNextPlayerId(next_player_id_);
+
+        for (const auto& dog_repr : dogs_) {
+            model::Dog dog = dog_repr.Restore();
+            model::Dog* ptr = session.RestoreDog(std::move(dog));
+            dog_id_map[dog_repr.GetDogId()] = ptr;
+        }
+
+        for (const auto& obj : lost_objects_) {
+            session.AddLostObject(obj.Restore());
+        }
+    }
+
     template <typename Archive>
-    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+    void serialize(Archive& ar, const unsigned) {
         ar& map_id_;
         ar& dogs_;
         ar& lost_objects_;
         ar& next_loot_id_;
-        ar& next_player_id_;  
+        ar& next_player_id_;
     }
-    
-    void Restore(model::Game& game, 
-             std::unordered_map<uint64_t, model::Dog*>& dog_id_map) const {
-    auto* map = game.FindMap(model::Map::Id{map_id_});
-    if (!map) {
-        throw std::runtime_error("Map not found: " + map_id_);
-    }
-    
-    auto& session = game.FindOrCreateSession(map);
-    session.SetNextLootId(next_loot_id_);
-    session.SetNextPlayerId(next_player_id_);
-    
 
-    for (const auto& dog_repr : dogs_) {
-        auto dog = dog_repr.Restore();
-        uint64_t dog_id = dog_repr.GetDogId();
-        session.RestoreDog(std::move(dog));
-        const auto& dogs = session.GetDogs();
-        if (!dogs.empty()) {
-            dog_id_map[dog_id] = dogs.back().get();
-        }
-    }
-    
-
-    for (const auto& obj_repr : lost_objects_) {
-        session.AddLostObject(obj_repr.Restore());
-    }
-}
-    
 private:
     std::string map_id_;
     std::vector<DogRepr> dogs_;
     std::vector<LostObjectRepr> lost_objects_;
     size_t next_loot_id_ = 0;
-    uint64_t next_player_id_ = 0;  
+    uint64_t next_player_id_ = 0;
 };
-
 
 struct PlayerTokenRepr {
     std::string token;
     uint64_t player_id;
     uint64_t dog_id;
     std::string map_id;
-    
+
     template <typename Archive>
-    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+    void serialize(Archive& ar, const unsigned) {
         ar& token;
         ar& player_id;
         ar& dog_id;
@@ -222,84 +221,77 @@ struct PlayerTokenRepr {
     }
 };
 
-
 class GameStateRepr {
 public:
     GameStateRepr() = default;
-    
-    explicit GameStateRepr(const model::Game& game, 
-                          const std::unordered_map<std::string, model::Player*>& tokens) {
+
+    GameStateRepr(const model::Game& game,
+                  const std::unordered_map<std::string, model::Player*>& tokens)
+    {
         for (const auto& [map, session] : game.GetSessions()) {
             sessions_.push_back(GameSessionRepr(*session));
         }
-        
+
         for (const auto& [token, player] : tokens) {
-            PlayerTokenRepr repr;
-            repr.token = token;
-            repr.player_id = player->GetId();
-            repr.dog_id = *player->GetDog()->GetId();
-            repr.map_id = *player->GetSession()->GetMap()->GetId();
-            tokens_.push_back(repr);
+            PlayerTokenRepr r;
+            r.token = token;
+            r.player_id = player->GetId();
+            r.dog_id = *player->GetDog()->GetId();
+            r.map_id = *player->GetSession()->GetMap()->GetId();
+            tokens_.push_back(r);
         }
     }
-   
-    void Restore(model::Game& game, 
+
+    void Restore(model::Game& game,
                  std::unordered_map<std::string, model::Player*>& tokens) const {
-        std::unordered_map<uint64_t, model::Dog*> dog_id_map;
-        for (const auto& session_repr : sessions_) {
-            session_repr.Restore(game, dog_id_map);
+
+        std::unordered_map<uint64_t, model::Dog*> dog_map;
+
+        for (const auto& s : sessions_) {
+            s.Restore(game, dog_map);
         }
 
-        for (const auto& token_repr : tokens_) {
-            const auto* map = game.FindMap(model::Map::Id{token_repr.map_id});
+        for (const auto& t : tokens_) {
+            auto* map = game.FindMap(model::Map::Id{t.map_id});
             if (!map) {
-                throw std::runtime_error("Map not found for token: " + token_repr.map_id);
+                throw std::runtime_error("Map not found: " + t.map_id);
             }
-            
-            auto& session = game.FindOrCreateSession(map);
-            
-            model::Player* found_player = nullptr;
 
-            for (auto* player : session.GetPlayers()) {
-                if (player->GetId() == token_repr.player_id) {
-                    found_player = player;
+            auto& session = game.FindOrCreateSession(map);
+
+            model::Player* player = nullptr;
+
+            for (auto* p : session.GetPlayers()) {
+                if (p->GetId() == t.player_id) {
+                    player = p;
                     break;
                 }
             }
-            
 
-            if (!found_player) {
-                const auto& dogs = session.GetDogs();
-                for (const auto& dog_ptr : dogs) {
-                    if (*dog_ptr->GetId() == token_repr.dog_id) {
-
-                        auto& player = session.AddPlayerWithId(*dog_ptr, token_repr.player_id);
-                        found_player = &player;
-                        break;
-                    }
+            if (!player) {
+                auto it = dog_map.find(t.dog_id);
+                if (it != dog_map.end()) {
+                    player = &session.AddPlayerWithId(*it->second, t.player_id);
                 }
             }
-            
-            if (found_player) {
-                tokens[token_repr.token] = found_player;
-            } else {
-                throw std::runtime_error("Failed to restore player for token: " + token_repr.token);
+
+            if (!player) {
+                throw std::runtime_error("Failed restore token: " + t.token);
             }
+
+            tokens[t.token] = player;
         }
     }
-    
+
     template <typename Archive>
-    void serialize(Archive& ar, [[maybe_unused]] const unsigned version) {
+    void serialize(Archive& ar, const unsigned) {
         ar& sessions_;
         ar& tokens_;
     }
-    
-    const std::vector<GameSessionRepr>& GetSessions() const { return sessions_; }
-    const std::vector<PlayerTokenRepr>& GetTokens() const { return tokens_; }
-    
+
 private:
     std::vector<GameSessionRepr> sessions_;
     std::vector<PlayerTokenRepr> tokens_;
 };
 
-}  // namespace serialization
+} // namespace serialization
